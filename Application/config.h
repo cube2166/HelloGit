8ee1e2a547e5eb9905d0c50acf3bf5d1eb9c9213
Jay <cube_leony@hotmail.com>
2023-05-03 14:11:08 +0800

    Hello Git!

---
+++ Application/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* Definition for the factory code and firmware version  */
+#define DEF_FIR_VER_LEN 8
+#define DEF_FAC_NUM_LEN 16
+#define DEF_DEVICE_CODE 0x38
+#define DEF_CANFW_VER "V0.05 "
+#define DEF_FW_VER "V00.5.02"          // 8 byte Firmware version
+#define DEF_FAC_NUM "YMC--SCCU-V20---" // 16 byte Factory number
+
+#endif //CONFIG_H_
