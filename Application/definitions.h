59fe8ac3214565b3351368c19db20ef75d528061
Jay <cube_leony@hotmail.com>
2023-05-03 14:13:52 +0800

    Hello 2

---
+++ Application/definitions.h
@@ -0,0 +1,23 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+#include "../Core/Inc/main.h"
+#include "../Core/Inc/fdcan.h"
+#include "../Core/Inc/gpio.h"
+#include "../Core/Inc/usart.h"
+#include "../Core/Inc/wwdg.h"
+
+#endif /* DEFINITIONS_H */
+/*******************************************************************************
+ End of File
+*/
+
